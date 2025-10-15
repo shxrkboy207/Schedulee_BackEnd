bed66e902944f5febe2cffaf114daaf4904c9e1d
shxrk <shxrk@DESKTOP-USRF71B>
2025-10-15 20:57:38 -0300

    Add project files.

---
+++ Schedulee/Models/Pessoa.cs
@@ -0,0 +1,15 @@
+﻿namespace Schedulee.Models
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public int Idade { get; set; }
+
+        public int CPF { get; set; }
+
+        public int Telefone { get; set; }
+    }
+}
